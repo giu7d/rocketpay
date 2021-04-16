861739d819aa42095672fa404b107a93ffcd9ff2
Giuseppe Setem <giuseppe7d@gmail.com>
2021-04-16 18:17:33 -0300

    Numbers demo

---
+++ test/rockertpay/number_test.exs
@@ -0,0 +1,28 @@
+defmodule Rocketpay.NumbersTest do
+  use ExUnit.Case
+
+  alias Rocketpay.Numbers
+
+  describe "sum_from_file/1" do
+    test "should return a valid value" do
+      expected_response = {:ok, 55}
+
+      "numbers"
+      |> Numbers.sum_from_file()
+      |> expect(expected_response)
+    end
+
+    test "should return a invalid value" do
+      expected_response = {:error, "Invalid File Name!"}
+
+      "wrong_name"
+      |> Numbers.sum_from_file()
+      |> expect(expected_response)
+    end
+  end
+
+
+  defp expect(resulted, expected) do
+    resulted == expected
+  end
+end
